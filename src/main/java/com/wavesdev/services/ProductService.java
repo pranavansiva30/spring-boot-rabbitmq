96265183ba49f57505401882dfcffb13aec3ee8f
vsivap2 <VSIVAP2@bitbucket.pearson.com>
2018-08-22 12:29:18 +0530

    spring boot rabbit mq

---
+++ src/main/java/com/wavesdev/services/ProductService.java
@@ -0,0 +1,24 @@
+package com.wavesdev.services;
+
+import com.wavesdev.domain.Product;
+import com.wavesdev.commands.ProductForm;
+
+import java.util.List;
+
+/**
+ * Created by jt on 1/10/17.
+ */
+public interface ProductService {
+
+    List<Product> listAll();
+
+    Product getById(Long id);
+
+    Product saveOrUpdate(Product product);
+
+    void delete(Long id);
+
+    Product saveOrUpdateProductForm(ProductForm productForm);
+
+    void sendProductMessage(String id);
+}
